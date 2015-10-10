8afeef1639d7db1d1315685ef95805da1efa8c16
Alexandr <Greyfix@yandex.ru>
2015-10-10 04:10:32 +0200

    Новый промежуточный класс в цепочке насследования от SpaceObject. В классе переопределяется абстрактный метод getType() родителя SpaceObject, но не переопределяется метод getInformation(). Он по-прежнему абстрактный.

--- SpaceObjects/Planets/Planet.java
+++ SpaceObjects/Planets/Planet.java
@@ -1,5 +1,18 @@
 package SpaceObjects.Planets;
 
-public interface Planet{
+import SpaceObjects.SpaceObject;
 
+public abstract class Planet extends SpaceObject{
+
+    protected Planet(String aName, int aCountSatellite, long aWeight, long aArea, int aLengthOfCircle, double aMaxTemperature, double aGravity) {
+        super(aName, aCountSatellite, aWeight, aArea, aLengthOfCircle, aMaxTemperature, aGravity);
+    }
+
+    @Override
+    public abstract String getInformation();
+
+    @Override
+    public String getType() {
+        return "Планета";
+    }
 }
--- SpaceObjects/Stars/Star.java
+++ SpaceObjects/Stars/Star.java
@@ -1,5 +1,18 @@
 package SpaceObjects.Stars;
 
-public interface Star{
+import SpaceObjects.SpaceObject;
 
+public abstract class Star extends SpaceObject {
+
+    protected Star(String aName,  int aCountSatellite, long aWeight, long aArea, int aLengthOfCircle, double aMaxTemperature, double aGravity) {
+        super(aName, aCountSatellite, aWeight, aArea, aLengthOfCircle, aMaxTemperature, aGravity);
+    }
+
+    @Override
+    public abstract String getInformation();
+
+    @Override
+    public String getType() {
+        return "Звезда";
+    }
 }
